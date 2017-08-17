6d2dbcb34b6bb1b915f79eb82102b6d02622e751
Mokar <tomsknets@yandex.ru>
2017-08-17 12:41:20 +0700

    add logic module client_logic_arm. Problem with tcpcache. THREAD PROBLEM and SOCKET num

--- arm/cashserver.c
+++ arm/cashserver.c
@@ -114,7 +114,14 @@ int main(int argc , char *argv[])
     {
         puts("Connection accepted");
         printf("(MAIN While) CLient socket ID[#%i] \n\r",client_sock); 
+		
+		
         pthread_t sniffer_thread;
+		 pthread_attr_t threadAttr;      // Set up detached thread attributes
+         pthread_attr_init(&threadAttr);
+         pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_DETACHED);
+
+		
         new_sock = malloc(1);
         *new_sock = client_sock;
 		
@@ -123,20 +130,22 @@ int main(int argc , char *argv[])
 		args[0].nSock = new_sock;
 		//args.pSignal = Signals;
 		args[0].SA_ptr = Signal_Array;
-        if( pthread_create ( &sniffer_thread, NULL,  connection_handler, (void*) &args[0]) < 0)
+		//if( pthread_create( &sniffer_thread, NULL,  connection_handler, (void*) &args[0]) < 0)
+        if( pthread_create( &sniffer_thread, &threadAttr,  connection_handler, (void*) &args[0]) < 0)
         {
             perror("SERVER: Could not create thread");
             return (ERROR_CREATE_THREAD);
         }
-        
         puts("\nSERVER: Handler assigned"); 
+		// Very HARD problem with thread resorces and thread termination
+		/*
         //Now join the thread , so that we dont terminate before the thread
         int status = pthread_join(sniffer_thread, (void**)&status_addr);
         if (status != SUCCESS) {
             printf("SERVER: Main error: can't join thread, status = %d\n", status);
             exit(ERROR_JOIN_THREAD);
         }
-		
+		*/
 		//printf("Last client by: %s \n", args.hello);
 		
     }
@@ -200,7 +209,7 @@ void ItoA(int n, char s[]) {
 /*
  * This will handle connection for each client
  * */
-void* connection_handler(void *args)
+void* connection_handler (void *args)
 {
 
 // from struct
@@ -267,13 +276,185 @@ int n=0;
 		iCmd1 = strstr (pCM,cmd_read_signal); // check for input cmd "signal_read"
 		iCmd2 = strstr (pCM,cmd_write_signal); // check for input cmd "signal_write"
 		iCmdEnd = strstr (pCM,cmd_end); // check for input cmd end ";"
-	//
+	//********************************* COMMAND SELECTION AND EXECUTION ******************************/
+if( iCmd1 != NULL ){ // cmd read_signal
+		speedtest_start();
+		    printf("[recived >CMD READ] read_signal \n\r");
+			size_t xx=0;
+			size_t cnt=0;
+			istr =strtok(client_message,":");
+			istr = strtok (NULL,":"); //mask or signal name
+			
+			// Выделение последующих частей
+			/*
+			while (istr != NULL)
+			     {
+			                // Вывод очередной выделенной части
+			                   printf ("EXPLODE: [ %s ]\n\r",istr);
+			                // Выделение очередной части строки
+			                istr = strtok (NULL,":");
+			     }
+			     */
+			printf ("NAME: [ %s ]\n\r",istr);
+			int found=0;  
+			char result[30000];  //buffer for response
+			strcpy (result,""); //erase buffer
+			for(cnt=0; cnt <  MAX_Signals; cnt++)
+			{
+				if( strstr(arg->SA_ptr[cnt].Name, istr))
+				{
+				 //      printf ("[%i] Signal Name: [%s]\t",cnt,arg->SA_ptr[cnt].Name); //debug
+				 //      printf (" ExState:    [%i] \n\r",arg->SA_ptr[cnt].ExState); //debug
+				        arg->SA_ptr[cnt].ExState=0; // Flag ExState turn off 
+				        strcpy(packed_txt_string,""); //erase buffer
+				        sSerial_by_num(cnt); //serialize to packet by number of signals				        
+				        strcat (result,packed_txt_string);
+				        //printf ("[ %s ]\n\r",packed_txt_string); //DEBUG
 
+					xx++;
+					found++;
+				}
+			}
+			
+			if (found == 0) {
+			printf("Signal not found \n\r");
+			
+			//arg->hello = "~core";
+			write(sock, mesErr, strlen(mesErr));
+			
+			memset(client_message, 0, mess_length);
+			close(*arg->nSock);
+			free((int*)arg->nSock);
+			pthread_exit(0);		
+			}
+
+			if (found > 0) {
+			printf("Signals READ  found [%i]! \n\r",found);
+			
+			//arg->hello = "~core";
+			strcat (result,mesOk); //add Ok to end
+			write(sock, result, strlen(result)); //send packet to client
+			}
+			/*
+			memset(client_message, 0, mess_length);
+			close(*arg->nSock);
+			free((int*)arg->nSock);
+			pthread_exit(0);		
+		        */
+		         printf(" ++++++++++++++++++++++++==>   SPEEDTEST TCPCache READ_REQ Time: [ %ld ] ms. \n\r", speedtest_stop());
+		         
+		}
+		
+		if( iCmd2 != NULL ){ // cmd write_signal
+		    speedtest_start();
+		    char digit[5]; //buffer Value of signal as CHAR
+		    char sname [100]; //buffer for temp store signal NAME
+		    char buf_signals[MAX_Signals][300]; //array of MAX_signal elements AND 300 characters each
+		    
+		    int val;
+		    printf("[recived >CMD WRITE] write_signal \n\r");
+			size_t xx=0;
+			size_t cnt=0;
+			int sn=1; //number of signals
+			            //istr =strtok(client_message,";");
+			            //strcpy (buf_signals[0],istr);
+			//istr = strtok (NULL,";");			
+			//printf ("EXPLODE NAME: [ %s ]\n\r",istr);
+			while ( istr != NULL ){
+			        /*if (istr == NULL) // defend from sigfault
+			        {
+			         printf("End write list\n\r");
+			         return ;
+			        }*/
+				istr =strtok(NULL,";");
+				/*
+				if ( istr != NULL ) // defend from sigfault
+			        {
+			         strcpy (buf_signals[sn],istr);
+				 printf ("[#%i] NAME: [%s] \n\r",sn,buf_signals[sn]);
+			         }
+			         */
+			         if (istr == NULL) // defend from sigfault
+			        {
+			         printf("End write list\n\r");
+			         // return ;
+			         break;
+			        } else {
+			    		strcpy (buf_signals[sn],istr);
+					//printf ("[#%i] NAME: [%s] \n\r",sn,buf_signals[sn]);
+			               }
+				
+				sn++;
+			}
+			
+			//printf("Start Write list parser\n\r");
+			int found=0;         
+			for(cnt=0; cnt < MAX_Signals; cnt++) //cycle for signals
+			{ 
+			
+			    int pr=0;
+			    if ( strlen ( arg->SA_ptr[cnt].Name ) > 2 ) {  // test if signal name not empty
+			             //printf ("Cyrrent SA: [%s] \n\r",arg->SA_ptr[cnt].Name);
+			             
+			         for (pr = 0; pr < sn; pr++){ //cycle for recived signals from client. number of recived signals = sn
+			
+			              
+			                //printf("[Total clientsignals#%i][#%i]Cyrrent client signal: [%s]\n\r",sn,pr,buf_signals[pr]);
+			                if ( strstr( buf_signals[pr], arg->SA_ptr[cnt].Name ) != NULL ) {
+			                    
+			                    //printf ("StrStr: [%s] [%s]\n\r",buf_signals[pr],arg->SA_ptr[cnt].Name);
+			                    found++;
+			                    istr =strtok(buf_signals[pr],":");	 // first element NAME        
+			                    if (istr != NULL){				         
+			                        istr = strtok (NULL,":");	 // second element Value
+				            //    printf ("Client Value: [ %s ]\n\r",istr);
+				                if (istr != NULL) strcpy(digit,istr);
+				                
+				                if (digit != NULL) val =  atoi(digit);
+				            //       printf("Value AtoI to write: [%i] \n\r",val);
+				                   arg->SA_ptr[cnt].Value[0] = val;
+				                   arg->SA_ptr[cnt].ExState = 1; //Flag value is changed
+				             }
+			                   }
+			            
+			          }
+			      
+			      pr++; // increment start index position of cycle for start next step exclude previus step!!!
+			    }
+			}
+			
+			
+			if (found == 0) {
+			printf("Signal not found!!! \n\r");
+			
+			//arg->hello = "~core";
+			write(sock, mesErr, strlen(mesErr));			
+			memset(client_message, 0, mess_length);
+			close(*arg->nSock);
+			free((int*)arg->nSock);
+			pthread_exit(0);		
+			}
+
+			if (found > 0) {
+			printf("Founded WRITE signals [%i]\n\r",found);
+			
+			//arg->hello = "~core";
+			write(sock, mesOk, strlen(mesOk));
+			}
+			/*
+			memset(client_message, 0, mess_length);
+			close(*arg->nSock);
+			free((int*)arg->nSock);
+			pthread_exit(0);		
+		       */
+		        printf(" ++++++++++++++++++++++++==>   SPEEDTEST TCPCache WRITE_REQ Time: [ %ld ] ms. \n\r", speedtest_stop());
+		        
+		}
 	
 		
 		
 		/****************************** ERROR CMD SECTION ********************************************/
-		/*
+		
 		if(  (iCmd1 == NULL) && (iCmd2 == NULL)  ){
 			printf("\nUnformatted Client Message\n");
 			//arg->msg = "~~~";
@@ -283,7 +464,7 @@ int n=0;
 			free((int*)arg->nSock);
 			pthread_exit(0);			
 		}		
-		*/
+		
     }
      
     if(read_size == 0)
